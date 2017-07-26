8451222ad14c4f42d8b9b948d6ff7c9c91752847
Mark Broski <mark@mbroski.com>
2017-07-26 12:49:46 -0400

    reformat itinerary view controller

--- iSEPTAv2/Storyboards/Schedules/Itinerary/ItineraryViewController.h
+++ iSEPTAv2/Storyboards/Schedules/Itinerary/ItineraryViewController.h
@@ -5,22 +5,16 @@
 //  Created by septa on 8/2/13.
 //  Copyright (c) 2013 SEPTA. All rights reserved.
 //
-
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
-
 #import <QuartzCore/CALayer.h>
 #import <QuartzCore/CAShapeLayer.h>
-
-
 // --==  Pods  ==--
 #import <SVProgressHUD/SVProgressHUD.h>
 #import <FMDB/FMDatabase.h>
 #import <Reachability/Reachability.h>
 #import <REMenu/REMenu.h>
 // --==  Pods  ==--
-
-
 // --==  Handy Helper Classes
 #import "TapableSegmentControl.h"
 #import "TabbedButton.h"
@@ -29,8 +23,6 @@
 #import "MenuAlertsImageView.h"
 #import "GTFSCommon.h"
 // --==  Handy Helper Classes
-
-
 // --==  Data Models  ==--
 #import "RouteData.h"
 #import "ItineraryObject.h"
@@ -39,8 +31,6 @@
 #import "DisplayedRouteData.h"
 #import "NTASaveObject.h"
 // --==  Data Models  ==--
-
-
 // --==  Xib Files
 #import "NextToArriveItineraryCell.h"
 #import "ItineraryCell.h"
@@ -48,30 +38,17 @@
 #import "ActiveTrainCell.h"
 #import "NTASaveObjectCell.h"
 // --==  Xib Files
-
-
 // --==  Connected VCs  ==--
 #import "StopNamesTableViewController.h"
 #import "FareViewController.h"
 #import "SystemAlertsViewController.h"
 #import "CommonWebViewController.h"
 // --==  Connected VCs  ==--
-
-
 // --==  Categories  ==--
-
 #import "UIColor+SEPTA.h"
 // --==  Categories  ==--
-
-
-#import "MapViewController.h"
-#import "TripDetailsTableController.h"
-
-
 #define DEFAULT_START_MESSAGE @"Select start"
 #define DEFAULT_END_MESSAGE   @"Select destination"
-
-
 // --==
 // --==  Alerts  ==--
 // --==
@@ -82,79 +59,53 @@
 //
 //#define REFRESH_NOW           @"refreshing now"
 //#define REFRESH_NO_CONNECTION @"no Internet connection detected"
-
 #define ALERTS_STARTUP    @"Fetching latest alerts..."
 #define ALERTS_EMPTY      @"No alerts available for this route"
 #define ALERTS_FOUND      @"Click to view details"
-
-
-
 //#define DEFAULT_MESSAGE @"Click to enter destination"
 #define CELL_REFRESH_RATE 60.0f/6.0f   // 6 times a minute, 10 seconds refresh rate
 #define JSON_REFRESH_RATE 60.0f/3.0f   // 3 times a minute, 20 seconds refresh rate
-
-
 //  --==
 //  --==  Enumerations
 //  --==
-typedef NS_ENUM(NSInteger, ItineraryFilterType)
-{
+typedef NS_ENUM(NSInteger, ItineraryFilterType) {
     kItineraryFilterTypeNow,
     kItineraryFilterTypeWeekday,
     kItineraryFilterTypeSat,
     kItineraryFilterTypeSun,
 };
-
-
 // --==
 // --==  Favorites  ==--
 // --==
 #define FAVORITE_SUBTITLE_ADD    @"add route to favorites"
 #define FAVORITE_SUBTITLE_REMOVE @"remove route from favorites"
 #define FAVORITE_SUBTITLE_NONE   @"no routes have been selected"
-typedef NS_ENUM(NSInteger, ItineraryFavoriteSubtitleState)
-{
+typedef NS_ENUM(NSInteger, ItineraryFavoriteSubtitleState) {
     kNextToArriveFavoriteSubtitleStatusUnknown,
     kNextToArriveFavoriteSubtitleAdded,
     kNextToArriveFavoriteSubtitleNotAdded,
 };
-
-typedef NS_ENUM(NSInteger, ItineraryDropDownMenuOrder)
-{
+typedef NS_ENUM(NSInteger, ItineraryDropDownMenuOrder) {
     kItineraryDropDownMenuOrderFavorites,
     kItineraryDropDownMenuOrderFare,
 };
 
-
-@interface ItineraryViewController : UIViewController <UIScrollViewDelegate, UITableViewDataSource, UITableViewDelegate, MKMapViewDelegate, ItineraryCellProtocol, StopNamesTableViewControllerProtocol, StopNamesForRouteDelegate, TabbedButtonProtocol, NextToArriveItineraryCellProtocol, GetAlertDataAPIProtocol, NSFileManagerDelegate>
-
-@property (weak, nonatomic) IBOutlet UILabel *lblTabbedLabel;
-@property (weak, nonatomic) IBOutlet UIImageView *imgTabbedLabel;
-@property (weak, nonatomic) IBOutlet UITableView *tableTrips;
-
-@property (nonatomic, strong) NSString *travelMode;
-@property (nonatomic, strong) RouteData *routeData;
-
-
-
-//@property (weak, nonatomic) IBOutlet UIBarButtonItem *btnFavorite;
-//@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentMapFavorite;
-
-
-//@property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
-//@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-//@property (weak, nonatomic) IBOutlet UITableView *tableSequence;
-
-
-//@property (weak, nonatomic) IBOutlet MKMapView *mapView;
-//@property (weak, nonatomic) IBOutlet UIView *viewTrips;
-//@property (weak, nonatomic) IBOutlet UIView *viewSequence;
-
-
-//@property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *leftSwipe;
-//@property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *rigthSwipe;
-//@property (strong, nonatomic) IBOutlet UITapGestureRecognizer *doubleTap;
-
-//@property (weak, nonatomic) IBOutlet TapableSegmentControl *segmentService;
-
+@interface ItineraryViewController : UIViewController <UIScrollViewDelegate, UITableViewDataSource, UITableViewDelegate, MKMapViewDelegate, ItineraryCellProtocol, StopNamesTableViewControllerProtocol, TabbedButtonProtocol, NextToArriveItineraryCellProtocol, GetAlertDataAPIProtocol, NSFileManagerDelegate>
+    @property(weak, nonatomic) IBOutlet UILabel *lblTabbedLabel;
+    @property(weak, nonatomic) IBOutlet UIImageView *imgTabbedLabel;
+    @property(weak, nonatomic) IBOutlet UITableView *tableTrips;
+    @property(nonatomic, strong) NSString *travelMode;
+    @property(nonatomic, strong) RouteData *routeData;
+    //@property (weak, nonatomic) IBOutlet UIBarButtonItem *btnFavorite;
+    //@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentMapFavorite;
+    //@property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
+    //@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+    //@property (weak, nonatomic) IBOutlet UITableView *tableSequence;
+    //@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+    //@property (weak, nonatomic) IBOutlet UIView *viewTrips;
+    //@property (weak, nonatomic) IBOutlet UIView *viewSequence;
+    //@property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *leftSwipe;
+    //@property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *rigthSwipe;
+    //@property (strong, nonatomic) IBOutlet UITapGestureRecognizer *doubleTap;
+    //@property (weak, nonatomic) IBOutlet TapableSegmentControl *segmentService;
 @end
