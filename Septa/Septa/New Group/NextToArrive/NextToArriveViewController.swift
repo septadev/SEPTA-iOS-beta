3ddb9035fbe946a39ec46ab5a6810f425d6aa997
Mark Broski <mark@mbroski.com>
2017-08-01 08:02:41 -0400

    scrolling toolbar

---
+++ Septa/Septa/New Group/NextToArrive/NextToArriveViewController.swift
@@ -0,0 +1,13 @@
+// SEPTA.org, created on 8/1/17.
+
+import UIKit
+
+class NextToArriveViewController: UIViewController {
+
+    @IBOutlet weak var toolbar: UIToolbar!
+
+    override func viewDidLoad() {
+        
+}
+
+}
