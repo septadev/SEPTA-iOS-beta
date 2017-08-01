e21971d83e81c4751decab5064a048ba6adfb95f
Mark Broski <mark@mbroski.com>
2017-08-01 07:16:14 -0400

    added septa schedule to project

--- SeptaCoreData/SeptaCoreData/PreloadedDatabaseMover.swift
+++ SeptaCoreData/SeptaCoreData/PreloadedDatabaseMover.swift
@@ -1,16 +1,17 @@
-// Septa.org created on 7/28/17.
+//// Septa.org created on 7/28/17.
+//
+//import Foundation
+//import Zip
+//
+//class PreloadedDatabaseMover {
+//    let importDatabase = ImportDatabase()
+//
+//    func expandZipDatabaseFile() {
+//        do {
+//            try Zip.unzipFile(importDatabase.preloadedZippedDatabaseURL!, destination: importDatabase.cachesDirectoryURL!, overwrite: true, password: nil, progress: nil)
+//        } catch {
+//            print("Something went wrong")
+//        }
+//    }
+//}
 
-import Foundation
-import Zip
-
-class PreloadedDatabaseMover {
-    let importDatabase = ImportDatabase()
-
-    func expandZipDatabaseFile() {
-        do {
-            try Zip.unzipFile(importDatabase.preloadedZippedDatabaseURL!, destination: importDatabase.cachesDirectoryURL!, overwrite: true, password: nil, progress: nil)
-        } catch {
-            print("Something went wrong")
-        }
-    }
-}
