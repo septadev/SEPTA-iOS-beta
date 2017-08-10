be5d8e9916f90a3079b4171ad81955e4e9ac8871
Mark Broski <mark@mbroski.com>
2017-08-10 12:52:16 -0400

    Add View Controller

--- iSEPTA/iSEPTA/Features/Schedules/TransitModesToolBarViewController.swift
+++ iSEPTA/iSEPTA/Features/Schedules/TransitModesToolBarViewController.swift
@@ -5,47 +5,82 @@ import UIKit
 import ReSwift
 
 class TransitModesToolbarViewController: UIViewController {
-
-    @IBOutlet weak var scrollbar: UIScrollView!
+    //    typealias StoreSubscriberStateType = TransitMode?
+    @IBOutlet var scrollbar: UIScrollView!
     @IBOutlet var transitModesToolbarElements: [TransitModeToolbarView]!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha: 1.000)
-        intializeToolbar()
-    }
-
-    func intializeToolbar() {
-
-        guard let preferredTransitMode = store.state.scheduleState.scheduleRequest?.transitMode else { return }
-        for toolbarItem in transitModesToolbarElements {
-            toolbarItem.highlighted = toolbarItem.transitMode == preferredTransitMode
-        }
-    }
-
-    @IBAction func ToolbarTapped(_ sender: UITapGestureRecognizer) {
-
-        guard !scrollbar.isDragging || !scrollbar.isDecelerating else { return }
-        let pt = sender.location(in: sender.view)
-        _ = transitModesToolbarElements.map { $0.highlighted = false }
-        let hitToolbar = transitModesToolbarElements.filter {
-
-            let contains = $0.frame.contains(pt)
-            return contains
-        }.first
-
-        hitToolbar?.highlighted = true
-
-        _ = transitModesToolbarElements.map { $0.setNeedsDisplay() }
-        dispatchAction(toolbarItem: hitToolbar)
-    }
-
-    func dispatchAction(toolbarItem: TransitModeToolbarView?) {
-        guard let transitMode = toolbarItem?.transitMode else { return }
-
-        let action = TransitModeSelected(transitMode: transitMode)
-        store.dispatch(action)
-        let preferenceAction = UserPreference.NewTransitModeAction(transitMode: transitMode)
-        store.dispatch(preferenceAction)
-    }
+    //
+    //    override func viewDidLoad() {
+    //        super.viewDidLoad()
+    //        view.backgroundColor = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha: 1.000) }
+    //
+    //    override func viewWillAppear(_ animated: Bool) {
+    //        //        subscribe()
+    //        super.viewWillAppear(animated)
+    //    }
+    //
+    //    override func didMove(toParentViewController _: UIViewController?) {
+    //        view.backgroundColor = UIColor.red
+    //    }
+    //
+    //    override func viewDidAppear(_: Bool) {
+    //        let count = view.subviews.count
+    //        let scount = scrollbar.subviews.count
+    //    }
+    //
+    //    func subscribe() {
+    //        store.subscribe(self) { subscription in
+    //            subscription.select(self.filterSubscription)
+    //        }
+    //    }
+    //
+    //    @IBAction func ToolbarTapped(_ sender: UITapGestureRecognizer) {
+    //
+    //        guard !scrollbar.isDragging || !scrollbar.isDecelerating else { return }
+    //        let pt = sender.location(in: sender.view)
+    //        _ = transitModesToolbarElements.map { $0.highlighted = false }
+    //        let hitToolbar = transitModesToolbarElements.filter {
+    //
+    //            let contains = $0.frame.contains(pt)
+    //            return contains
+    //        }.first
+    //
+    //        hitToolbar?.highlighted = true
+    //
+    //        _ = transitModesToolbarElements.map { $0.setNeedsDisplay() }
+    //        dispatchAction(toolbarItem: hitToolbar)
+    //    }
+    //
+    //    override func viewWillDisappear(_ animated: Bool) {
+    //        store.unsubscribe(self)
+    //        super.viewWillDisappear(animated)
+    //    }
+    //
+    //    func dispatchAction(toolbarItem: TransitModeToolbarView?) {
+    //        guard let transitMode = toolbarItem?.transitMode else { return }
+    //
+    //        let action = TransitModeSelected(transitMode: transitMode)
+    //        store.dispatch(action)
+    //        let preferenceAction = UserPreference.NewTransitModeAction(transitMode: transitMode)
+    //        store.dispatch(preferenceAction)
+    //    }
+    //
+    //    func filterSubscription(state: AppState) -> TransitMode? {
+    //        return state.scheduleState.scheduleRequest?.transitMode
+    //    }
+    //
+    //    func newState(state: StoreSubscriberStateType) {
+    //        guard let transitMode = state else { fatalError("message: there should always be a transit mode") }
+    //        // intializeToolbar(transitMode: transitMode)
+    //    }
+    //
+    //    func intializeToolbar(transitMode _: TransitMode) {
+    //        let count = transitModesToolbarElements.count
+    //        for toolbarItem in transitModesToolbarElements {
+    //            //            toolbarItem.highlighted = toolbarItem.transitMode == transitMode
+    //        }
+    //    }
+    //
+    //    deinit {
+    //        _ = 1
+    //    }
 }
