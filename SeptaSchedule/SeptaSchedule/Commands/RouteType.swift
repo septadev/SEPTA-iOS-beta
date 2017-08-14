d57a1dc13cb2c4f4f47f9367d8d87fda61c9a7ae
Mark Broski <mark@mbroski.com>
2017-08-13 21:58:14 -0400

    project clean up

--- SeptaSchedule/SeptaSchedule/Commands/RouteType.swift
+++
@@ -1,9 +0,0 @@
-// Septa. 2017
-
-import Foundation
-
-public enum RouteType: Int {
-
-    case bus = 3
-    case trolley = 0
-}
---
+++ SeptaSchedule/SeptaSchedule/Commands/TripStart/TripStartCommand.swift
@@ -0,0 +1,9 @@
+//
+//  TripStartCommand.swift
+//  SeptaSchedule
+//
+//  Created by Mark Broski on 8/13/17.
+//  Copyright © 2017 Mark Broski. All rights reserved.
+//
+
+import Foundation
