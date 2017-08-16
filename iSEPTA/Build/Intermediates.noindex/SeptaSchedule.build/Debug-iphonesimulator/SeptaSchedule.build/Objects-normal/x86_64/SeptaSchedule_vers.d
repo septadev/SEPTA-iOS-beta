ae1e3ca886f6a65028ee924c61d46efc9d2f7f69
Mark Broski <mark@mbroski.com>
2017-08-15 21:30:54 -0400

    throwing everythng up there

---
+++ iSEPTA/Build/Intermediates.noindex/SeptaSchedule.build/Debug-iphonesimulator/SeptaSchedule.build/Objects-normal/x86_64/SeptaSchedule_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/mbroski/Code/Septa/Septa2/iSEPTA/Build/Intermediates.noindex/SeptaSchedule.build/Debug-iphonesimulator/SeptaSchedule.build/DerivedSources/SeptaSchedule_vers.c
---
+++ iSEPTA/Build/Intermediates.noindex/iSEPTA.build/Debug-iphonesimulator/iSEPTA.build/Script-585F585A1F3377EF00D3ED6C.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+carthage copy-frameworks
