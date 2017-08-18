e51b54ae00e2fe55f83a0a839eeadfe5d8ae7f09
Mark Broski <mark@mbroski.com>
2017-08-18 19:33:47 -0400

    code clean up

--- iSEPTA/iSEPTA/App State/Schedule/Reducers/ScheduleReducer.swift
+++ iSEPTA/iSEPTA/App State/Schedule/Reducers/ScheduleReducer.swift
@@ -9,14 +9,11 @@ fileprivate typealias Data = ScheduleDataReducer
 struct ScheduleReducer {
 
     static func main(action: Action, state: ScheduleState?) -> ScheduleState {
-        if let action = action as? ReverseLoaded {
-
-            return ScheduleState(
-                scheduleRequest: action.scheduleRequest,
-                scheduleData: ScheduleData(availableRoutes: state?.scheduleData?.availableRoutes, availableStarts: nil, availableStops: nil, availableTrips: nil, errorString: action.error)
-            )
+        /// Handle the edge case of trip reverses
+        if let action = action as? ReverseLoaded, state = state {
+            reduceTripReverse(action: action, state, state)
         }
-
+        /// only work with non optionals
         if let newState = state {
             guard let action = action as? ScheduleAction,
                 let scheduleRequest = newState.scheduleRequest,
@@ -27,7 +24,7 @@ struct ScheduleReducer {
                 scheduleRequest: Req.reduceRequest(action: action, scheduleRequest: scheduleRequest),
                 scheduleData: Data.reduceData(action: action, scheduleData: scheduleData)
             )
-
+        /// if there are optionals, run through the init
         } else {
             return ScheduleState(
                 scheduleRequest: Req.initRequest(),
@@ -35,4 +32,16 @@ struct ScheduleReducer {
             )
         }
     }
+    
+    static func reduceTripReverse(action: TripReverse, state: ScheduleState) -> ScheduleState{
+        return ScheduleState(
+            scheduleRequest: action.scheduleRequest,
+            scheduleData: ScheduleData(availableRoutes: state?.scheduleData?.availableRoutes, availableStarts: nil, availableStops: nil, availableTrips: nil, errorString: action.error)
+        )
+
+        
+    }
 }
+
+
+
