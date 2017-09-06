e6ac27707ffc8106e47c3398adbd3d54de9825a2
Mark Broski <mark@mbroski.com>
2017-09-06 17:01:51 -0400

    creating basic controllers

---
+++ iSEPTA/iSEPTA/Features/NextToArrive/Maps/NextToArriveInfoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  NextToArriveInfoViewController.swift
+//  iSEPTA
+//
+//  Created by Mark Broski on 9/6/17.
+//  Copyright © 2017 Mark Broski. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NextToArriveInfoViewController: UIViewController {
+
+
+}
---
+++ iSEPTA/iSEPTA/Features/NextToArrive/Maps/NextToArriveMapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NextToArriveMapViewController.swift
+//  iSEPTA
+//
+//  Created by Mark Broski on 9/6/17.
+//  Copyright © 2017 Mark Broski. All rights reserved.
+//
+
+import Foundation
+import AEXML
+import UIKit
+
+
+class NextToArriveMapViewController: UIViewController {
+
+
+}
+
