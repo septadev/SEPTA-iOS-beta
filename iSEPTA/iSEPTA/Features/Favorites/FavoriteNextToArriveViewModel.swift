2f1c28a0e2d72ad3bc7c6d53ef7794d06ce8838d
Mark Broski <mark@mbroski.com>
2017-09-13 21:59:03 -0400

    create favorite info view model

---
+++ iSEPTA/iSEPTA/Features/Favorites/FavoriteNextToArriveViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  FavoriteNextToArriveViewModel.swift
+//  iSEPTA
+//
+//  Created by Mark Broski on 9/13/17.
+//  Copyright © 2017 Mark Broski. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SeptaSchedule
+
+class FavoriteNextToArriveViewModel: BaseNextToArriveInfoViewModel {
+    let favorite: Favorite
+    init(favorite: Favorite, delegate: UpdateableFromViewModel) {
+        self.favorite = favorite
+        super.init()
+
+        groupedTripData = NextToArriveGrouper.buildNextToArriveTripSections(trips: favorite.nextToArriveTrips)
+        self.delegate = delegate
+    }
+
+    override func transitMode() -> TransitMode {
+        return favorite.transitMode
+    }
+
+    func viewTitle() -> String {
+        return scheduleRequest().transitMode.nextToArriveInfoDetailTitle()
+    }
+
+    override func subscribe() {
+    }
+}
--- iSEPTA/iSEPTA/Features/Favorites/FavoritesViewModel.swift
+++ iSEPTA/iSEPTA/Features/Favorites/FavoritesViewModel.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+import UIKit
+import ReSwift
+import SeptaSchedule
+
+class FavoritesViewModel: BaseNextToArriveInfoViewModel, StoreSubscriber, SubscriberUnsubscriber {
+
+    typealias StoreSubscriberStateType = [Favorite]
+
+    override func scheduleRequest() -> ScheduleRequest {
+        return store.state.nextToArriveState.scheduleState.scheduleRequest
+    }
+
+    override func transitMode() -> TransitMode {
+        return scheduleRequest().transitMode
+    }
+
+    func newState(state _: StoreSubscriberStateType) {
+    }
+
+    func viewTitle() -> String {
+        return scheduleRequest().transitMode.nextToArriveInfoDetailTitle()
+    }
+
+    override func subscribe() {
+        store.subscribe(self) {
+            $0.select {
+                $0.favoritesState.favorites
+            }.skipRepeats { $0 == $1 }
+        }
+    }
+
+    func unsubscribe() {
+        store.unsubscribe(self)
+    }
+}
