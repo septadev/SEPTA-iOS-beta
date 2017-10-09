535c223f24d5d9a9095b3a9df1a5e044319fefd7
Mark Broski <mark@mbroski.com>
2017-10-08 22:26:14 -0400

    completed about feature

--- iSEPTA/iSEPTA/Features/More/About/AboutViewController.swift
+++ iSEPTA/iSEPTA/Features/More/About/AboutViewController.swift
@@ -9,11 +9,29 @@
 import Foundation
 import UIKit
 
-class AboutViewController: UIViewController, IdentifiableController {
+class AboutViewController: UIViewController, IdentifiableController, UITextViewDelegate {
     var viewController: ViewController = .aboutViewController
-
+    var openState: Bool = false
+    @IBOutlet weak var attributionsView: UIView!
+    @IBOutlet weak var attributionsLabel: UILabel!
     @IBOutlet weak var appInformationView: UIView!
     @IBOutlet weak var appInfoStackView: UIStackView!
+
+    @IBOutlet weak var plusButton: AlertDetailButton!
+
+    @IBOutlet weak var licenseTextViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var licenseTextView: UITextView! {
+        didSet {
+            licenseTextView.delegate = self
+        }
+    }
+
+    @IBOutlet weak var attributionsHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var attributionsTextView: UITextView! {
+        didSet {
+            attributionsTextView.delegate = self
+        }
+    }
 
     var viewModel: AboutViewModel!
 
@@ -29,6 +47,15 @@ class AboutViewController: UIViewController, IdentifiableController {
         viewModel = AboutViewModel()
         buildAppInfo()
         UIView.addSurroundShadow(toView: appInformationView, withCornerRadius: 3)
+        UIView.addSurroundShadow(toView: attributionsView, withCornerRadius: 3)
+        attributionsHeightConstraint.constant = 0
+        setLicenseHeight()
+    }
+
+    func setLicenseHeight() {
+        let windowWidth = UIScreen.main.bounds.width - 30
+        let sizeThatFitsTextView = licenseTextView.sizeThatFits(CGSize(width: windowWidth, height: CGFloat(MAXFLOAT)))
+        licenseTextViewHeightConstraint.constant = sizeThatFitsTextView.height
     }
 
     func buildAppInfo() {
@@ -39,4 +66,50 @@ class AboutViewController: UIViewController, IdentifiableController {
             appInfoStackView.addArrangedSubview(appInfoView)
         }
     }
+
+    @IBAction func openButtonTapped(_: Any) {
+        openState = !openState
+
+        calculateFittingSize()
+    }
+
+    func calculateFittingSize() {
+        guard let view = self.view else { return }
+        let newTitleText: String
+        if openState {
+            let windowWidth = UIScreen.main.bounds.width - 60
+            let sizeThatFitsTextView = attributionsTextView.sizeThatFits(CGSize(width: windowWidth, height: CGFloat(MAXFLOAT)))
+            let heightOfText = sizeThatFitsTextView.height
+
+            attributionsHeightConstraint.constant = heightOfText
+            newTitleText = "Hide Attributions"
+
+        } else {
+            attributionsHeightConstraint.constant = 0
+            newTitleText = "View Attributions"
+        }
+        view.setNeedsLayout()
+        UIView.animate(withDuration: 0.25, animations: { view.layoutIfNeeded() }, completion: { [weak self] _ in
+            guard let strongSelf = self else { return }
+            strongSelf.attributionsLabel.text = newTitleText
+            strongSelf.plusButton.isOpen = strongSelf.openState
+        })
+    }
+
+    func textView(_: UITextView, shouldInteractWith URL: URL, in _: NSRange) -> Bool {
+
+        return true
+    }
+
+    //
+    //    var fittingHeight: CGFloat {
+    //        if pinkAlertHeaderView.actionButton.isOpen {
+    //            let windowWidth = UIScreen.main.bounds.width - 50
+    //            let sizeThatFitsTextView = textView.sizeThatFits(CGSize(width: windowWidth, height: CGFloat(MAXFLOAT)))
+    //            let heightOfText = sizeThatFitsTextView.height
+    //            return heightOfText + 75
+    //        } else {
+    //            return 75
+    //        }
+    //    }
 }
