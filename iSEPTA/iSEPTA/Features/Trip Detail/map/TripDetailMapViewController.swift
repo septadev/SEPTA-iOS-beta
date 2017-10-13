e85a086dec79b6c9deee01c3bf3a3fab7f1bda1e
Mark Broski <mark@mbroski.com>
2017-10-13 02:24:14 -0400

    Configured MapViewController

--- iSEPTA/iSEPTA/Features/Trip Detail/map/TripDetailMapViewController.swift
+++ iSEPTA/iSEPTA/Features/Trip Detail/map/TripDetailMapViewController.swift
@@ -8,6 +8,101 @@
 
 import Foundation
 import UIKit
+import MapKit
+import SeptaSchedule
 
-class NextToArriveDetailMapViewController: UIViewController {
+class TripDetailMapViewController: UIViewController, TripDetailState_TripDetailsWatcherDelegate {
+
+    @IBOutlet var mapView: MKMapView!
+
+    var tripDetailWatcher: TripDetailState_TripDetailsWatcher?
+    var vehiclesAnnotationsAdded = [VehicleLocationAnnotation]()
+    let mapViewDelegate = TripDetailMapViewDelegate()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = mapViewDelegate
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tripDetailWatcher = TripDetailState_TripDetailsWatcher()
+        tripDetailWatcher?.delegate = self
+        showAnotations()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tripDetailWatcher = nil
+    }
+
+    func tripDetailState_TripDetailsUpdated(nextToArriveStop: NextToArriveStop) {
+        drawRoute(routeId: nextToArriveStop.routeId)
+        drawVehicle(nextToArriveStop: nextToArriveStop)
+    }
+
+    var routeHasBeenAdded = false
+    func drawRoute(routeId: String) {
+        guard !routeHasBeenAdded else { return }
+        guard let url = locateKMLFile(routeId: routeId) else { return }
+        parseKMLForRoute(url: url, routeId: routeId)
+        routeHasBeenAdded = true
+    }
+
+    func parseKMLForRoute(url: URL, routeId: String) {
+        print("Beginning to parse")
+        KMLDocument.parse(url) { [weak self] kml in
+            guard let overlays = kml.overlays as? [KMLOverlayPolyline] else { return }
+            if let routeOverlays = self?.mapOverlaysToRouteOverlays(routeId: routeId, overlays: overlays) {
+                self?.mapView.addOverlays(routeOverlays)
+            }
+        }
+    }
+
+    func locateKMLFile(routeId: String) -> URL? {
+        guard let url = Bundle.main.url(forResource: routeId, withExtension: "kml") else { return nil }
+        if FileManager.default.fileExists(atPath: url.path) {
+            return url
+        } else {
+            print("Could not find kml file for route \(routeId)")
+            return nil
+        }
+    }
+
+    func mapOverlaysToRouteOverlays(routeId: String, overlays: [KMLOverlayPolyline]) -> [RouteOverlay] {
+        return overlays.map { overlay in
+            let routeOverlay = RouteOverlay(points: overlay.points(), count: overlay.pointCount)
+            routeOverlay.routeId = routeId
+            return routeOverlay
+        }
+    }
+
+    func drawVehicle(nextToArriveStop: NextToArriveStop) {
+        guard let vehicleLocationCoordinate = nextToArriveStop.vehicleLocationCoordinate else { return }
+        clearExistingVehicleLocations()
+        let vehicleLocation = VehicleLocation(location: vehicleLocationCoordinate, nextToArriveStop: nextToArriveStop)
+
+        let annotation = VehicleLocationAnnotation(vehicleLocation: vehicleLocation)
+        annotation.coordinate = vehicleLocationCoordinate
+        if #available(iOS 11.0, *) {
+            annotation.title = nil
+        } else {
+            annotation.title = nextToArriveStop.transitMode.mapTitle()
+        }
+
+        mapView.addAnnotation(annotation)
+        vehiclesAnnotationsAdded.append(annotation)
+        showAnotations()
+    }
+
+    func showAnotations() {
+        mapView.showAnnotations(mapView.annotations, animated: false)
+
+        mapView.setVisibleMapRect(mapView.visibleMapRect, edgePadding: UIEdgeInsets(top: 25, left: 0, bottom: 25, right: 0), animated: true)
+    }
+
+    func clearExistingVehicleLocations() {
+        mapView.removeAnnotations(vehiclesAnnotationsAdded)
+        vehiclesAnnotationsAdded.removeAll()
+    }
 }
---
+++ iSEPTA/iSEPTA/Features/Trip Detail/map/TripDetailMapViewDelegate.swift
@@ -0,0 +1,163 @@
+//
+//  TripDetailMapViewDelegate.swift
+//  iSEPTA
+//
+//  Created by Mark Broski on 10/13/17.
+//  Copyright © 2017 Mark Broski. All rights reserved.
+//
+
+import Foundation
+import MapKit
+import SeptaSchedule
+import SeptaRest
+
+class TripDetailMapViewDelegate: NSObject, MKMapViewDelegate {
+
+    var tripDetails: NextToArriveStop?
+
+    func mapView(_: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        guard let routeOverlay = overlay as? RouteOverlay, let routeId = routeOverlay.routeId else { return MKOverlayRenderer(overlay: overlay) }
+        let renderer: MKPolylineRenderer = MKPolylineRenderer(polyline: routeOverlay)
+
+        renderer.strokeColor = Route.colorForRouteId(routeId, transitMode: .bus)
+        renderer.lineWidth = 2.0
+
+        return renderer
+    }
+
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        switch annotation {
+
+        case let annotation as VehicleLocationAnnotation:
+            return retrieveVehicleAnnotationView(annotation: annotation, mapView: mapView)
+        default:
+            return nil
+        }
+    }
+
+    func retrieveVehicleAnnotationView(annotation: VehicleLocationAnnotation, mapView: MKMapView) -> MKAnnotationView {
+        let vehicleId = "vehicle"
+        let annotationView: MKAnnotationView
+        if let vehicleView = mapView.dequeueReusableAnnotationView(withIdentifier: vehicleId){
+            annotationView = vehicleView
+        } else {
+            annotationView = buildNewVehicleAnnotationView(annotation: annotation, vehicleViewId: vehicleId)
+        }
+
+        buildVehicleTitle(vehicleLocation: annotation.vehicleLocation, calloutView: annotationView.detailCalloutAccessoryView)
+        annotationView.annotation = annotation
+        //        if annotation.vehicleLocation.isMoving, let bearing = annotation.vehicleLocation.bearing {
+        //            let startingTransform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+        //            let transformForBearing = startingTransform.rotated(by: CGFloat(bearing))
+        //            //            vehicleView.transform = transformForBearing
+        //        }
+        return annotationView
+    }
+
+    func buildVehicleTitle(vehicleLocation: VehicleLocation, calloutView: UIView?) {
+        guard let calloutView = calloutView as? MapVehicleCalloutView , let transitMode = tripDetails?.transitMode else { return }
+
+        if let detail = vehicleLocation.nextToArriveStop.nextToArriveDetail as? NextToArriveRailDetails,
+            let consist = detail.consist, let destination = detail.destination, let delay = detail.destinationDelay, let tripId = detail.tripid {
+                let countString = String(consist.count)
+                let delayString = buildDelayString(delay:delay)
+                calloutView.label1.text = "Train: #\(tripId) to \(destination)"
+                calloutView.label2.text = delayString
+                calloutView.label3.text = "# of Train Cars: \(countString)"
+                return
+            }
+        if let detail = vehicleLocation.nextToArriveStop.nextToArriveDetail as? NextToArriveBusDetails,
+            let vehicleId = detail.vehicleid, let destination = detail.destinationStation, let delay = detail.destinationDelay, let blockId = detail.blockid {
+
+                let delayString = buildDelayString(delay:delay)
+                calloutView.label1.text = "Block ID: \(blockId) to \(destination)"
+                calloutView.label2.text = "Vehicle Number: \(vehicleId)"
+                calloutView.label3.text = delayString
+                return
+            }
+        let stop = vehicleLocation.nextToArriveStop
+
+        if transitMode.useBusForDetails(){
+           if let lastStopName = stop.lastStopName {
+                calloutView.label1.text = "#\(stop.routeId):  to \(lastStopName)"
+           } else {
+                calloutView.label1.text = ""
+           }
+
+           if let vehicleIds = stop.vehicleIds, let firstVehicle = vehicleIds.first {
+                calloutView.label2.text = "Vehicle Number: \(firstVehicle)"
+           } else {
+                calloutView.label3.text = "Vehicle Number not available"
+           }
+
+           if let delay = stop.delayMinutes {
+                let delayString = buildDelayString(delay:delay)
+                calloutView.label3.text = delayString
+           } else {
+                calloutView.label3.text = "Status: No Realtime data"
+           }
+            return
+        }
+
+           if transitMode.useRailForDetails(){
+           if let  tripId = stop.tripId , let lastStopName = stop.lastStopName{
+                calloutView.label1.text = "Train: #\(tripId) to \(lastStopName)"
+           } else {
+                calloutView.label1.text = ""
+           }
+
+        if let delay = stop.delayMinutes {
+                let delayString = buildDelayString(delay:delay)
+                calloutView.label2.text = delayString
+           } else {
+                calloutView.label2.text = "Status: No Realtime data"
+           }
+
+           if let vehicleIds = stop.vehicleIds {
+                calloutView.label3.text = "# of Train Cars: \(vehicleIds.count)"
+           } else {
+                calloutView.label3.text = "# of Train Cars: unknown"
+           }
+
+         return
+
+        }
+
+          calloutView.label1.text = ""
+                calloutView.label2.text = ""
+                calloutView.label3.text = ""
+
+    }
+
+    func buildDelayString(delay: Int) -> String {
+        let delayString: String
+                switch delay {
+                    case let delay where delay < 0:
+                    delayString = "Status: \(delay) min early"
+                    case 0:
+                    delayString = "Status: On Time"
+                    case  let delay where delay > 0:
+                    delayString = "Status: \(delay) min late"
+                    default:
+                    delayString = ""
+                }
+        return delayString
+    }
+
+    func buildNewVehicleAnnotationView(annotation: VehicleLocationAnnotation, vehicleViewId: String) -> MKAnnotationView {
+        guard let transitMode = tripDetails?.transitMode else { return MKAnnotationView() }
+        let vehicleView = MKAnnotationView(annotation: annotation, reuseIdentifier: vehicleViewId)
+        vehicleView.image = transitMode.mapPin()
+        vehicleView.canShowCallout = true
+        vehicleView.detailCalloutAccessoryView = UIView.loadNibView(nibName: "MapVehicleCalloutView")!
+        return vehicleView
+    }
+
+    func mapView(_: MKMapView, didSelect annotationView: MKAnnotationView) {
+        print("User Selected a pin")
+    }
+
+    func mapViewDidFinishLoadingMap(_: MKMapView) {
+        print("Map View Finished Loading")
+    }
+}
