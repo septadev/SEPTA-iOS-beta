5900c7914aaf8aadbf3f0ea9a85d6e078b081c6a
Mark Broski <mark@mbroski.com>
2017-11-01 04:48:12 -0400

    Update swift format

--- Utilities/swiftFormat.sh
+++ Utilities/swiftFormat.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-cd /Volumes/Docs/markbroski/Code/SEPTA-iOS
 
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR/.."
 /usr/local/bin/swiftformat ./iSEPTA/
 /usr/local/bin/swiftformat ./SeptaSchedule/ 
 /usr/local/bin/swiftformat ./SeptaRest/ 
