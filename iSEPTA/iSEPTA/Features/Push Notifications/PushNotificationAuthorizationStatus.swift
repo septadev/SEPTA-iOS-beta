b9e4d78fe04ffb8e7c17e011d919168c42306bb2
Mark Broski <mark@mbroski.com>
2018-07-27 05:56:27 -0400

    Push Notification Authorization Status

---
+++ iSEPTA/iSEPTA/Features/Push Notifications/PushNotificationAuthorizationStatus.swift
@@ -0,0 +1,24 @@
+//
+//  PushNotificationAuthorizationStatus.swift
+//  iSEPTA
+//
+//  Created by Mark Broski on 7/24/18.
+//  Copyright © 2018 Mark Broski. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
+
+enum PushNotificationAuthorizationState: String, Codable, Equatable {
+    case notDetermined
+    case denied
+    case authorized
+
+    init(status: UNAuthorizationStatus) {
+        switch status {
+        case .notDetermined: self = .notDetermined
+        case .denied: self = .denied
+        case .authorized: self = .authorized
+        }
+    }
+}
