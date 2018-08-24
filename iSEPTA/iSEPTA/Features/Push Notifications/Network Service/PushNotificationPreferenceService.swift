e6f9f4d88890ea9f78078af3914f2911c2d323c9
Mike Mannix <mike.mannix@gmail.com>
2018-08-24 15:00:13 -0400

    Push notifications API integration

--- iSEPTA/iSEPTA/Features/Push Notifications/Network Service/PushNotificationPreferenceService.swift
+++ iSEPTA/iSEPTA/Features/Push Notifications/Network Service/PushNotificationPreferenceService.swift
@@ -6,32 +6,58 @@
 //  Copyright © 2018 Mark Broski. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct PushNotificationPreferenceService {
     static func post(state: PushNotificationPreferenceState) {
-        var postBody: PushNotificationPreferencePostBody
-        if state.userWantsToEnablePushNotifications {
-            postBody = convertStateToPostBody(state: state)
-        } else {
-            postBody = PushNotificationPreferencePostBody(deviceId: state.deviceId, regToken: state.firebaseToken, specialAnnouncements: false, timeWindows: nil, routeSubscriptions: nil)
+        let body = postBody(state: state)
+
+        guard let baseUrl = Bundle.main.object(forInfoDictionaryKey: "septaBaseUrl") as? String,
+            let url = URL(string: "\(baseUrl)/pushnotification/subscription"),
+            let httpBody = try? JSONEncoder().encode(body),
+            let apiKey = Bundle.main.object(forInfoDictionaryKey: "septaApiKey") as? String else {
+            store.dispatch(PushNotificationPreferenceSynchronizationFail())
+            return
         }
 
-        print(":M: making api call to save push notif pref")
-        print(":M: deviceId: \(postBody.deviceId)")
-        print(":M: token: \(postBody.regToken)")
-        print(":M: specials: \(postBody.specialAnnouncements)")
-        print(":M: routes: \(postBody.routeSubscriptions ?? [])")
-        print(":M: windows: \(postBody.timeWindows ?? [])")
+        let request = buildRequest(url: url, key: apiKey, body: httpBody)
+        let session = URLSession.shared
 
-        store.dispatch(PushNotificationPreferenceSynchronizationSuccess())
+        DispatchQueue.main.async {
+            UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        }
 
-        // If success
-        //   dispatch PushNotificationPreferenceSynchronizationSuccess()
-        //     this will set syncStatus to .upToDate
-        // Else
-        //   dispatch PushNotificationPreferenceSynchronizationFail()
-        //     this will copy the lastSync'd property to pushPrefState and set syncStatus to .upToDate
+        session.dataTask(with: request) { _, response, error in
+            DispatchQueue.main.async {
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            }
+            if error != nil {
+                DispatchQueue.main.async {
+                    store.dispatch(PushNotificationPreferenceSynchronizationFail())
+                }
+            } else {
+                DispatchQueue.main.async {
+                    store.dispatch(PushNotificationPreferenceSynchronizationSuccess())
+                }
+            }
+        }.resume()
+    }
+
+    private static func buildRequest(url: URL, key: String, body: Data) -> URLRequest {
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("Application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue(key, forHTTPHeaderField: "x-api-key")
+        request.httpBody = body
+        return request
+    }
+
+    private static func postBody(state: PushNotificationPreferenceState) -> PushNotificationPreferencePostBody {
+        if state.userWantsToEnablePushNotifications {
+            return convertStateToPostBody(state: state)
+        } else {
+            return PushNotificationPreferencePostBody(deviceId: state.deviceId, regToken: state.firebaseToken, specialAnnouncements: false, timeWindows: nil, routeSubscriptions: nil)
+        }
     }
 
     private static func convertStateToPostBody(state: PushNotificationPreferenceState) -> PushNotificationPreferencePostBody {
